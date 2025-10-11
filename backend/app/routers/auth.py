d9cbd8568aa51e4d0c5bb1c06009d971f1d89c71
Ganesan2006 <ganesant2007@gmail.com>
2025-10-11 13:55:26 +0530

    Update auth.py

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -2,9 +2,9 @@
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from backend.app.models import schemas, database_models
-from backend.app.database.database_config import get_db
-from backend.app.core.security import (
+from app.models import schemas, database_models
+from app.database.database_config import get_db
+from app.core.security import (
     get_password_hash,
     verify_password,
     create_access_token,
